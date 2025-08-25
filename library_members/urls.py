15dfeff6dc0d6d610de79f9b9ac89f6d46984bd6
473krija <krijashobi2003@gmail.com>
2025-08-25 17:33:55 +0530

    updating the files with html files

--- library_members/urls.py
+++ library_members/urls.py
@@ -10,4 +10,14 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/fines/', FineListCreateView.as_view(), name='fine_list_create'),
     path('api/fines/<int:pk>/', FineDetailView.as_view(), name='fine_detail'),
+
+    # HTML Template Views
+    path('memberprofiles/', member_list, name='member_list'),
+    path('memberprofiles/<int:pk>/', member_detail, name='member_detail'),
+
+    path('readingclubs/', club_list, name='club_list'),
+    path('readingclubs/<int:pk>/', club_detail, name='club_detail'),
+
+    path('fines/', fine_list, name='fine_list'),
+    path('fines/<int:pk>/', fine_detail, name='fine_detail'),
 ]
--- library_members/views.py
+++ library_members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import *
 from .serializers import *
 from rest_framework import viewsets
+from django.shortcuts import render, get_object_or_404, redirect
 
 class MemberProfileViewSet(viewsets.ModelViewSet):
     queryset = MemberProfile.objects.all()
@@ -24,4 +25,39 @@ class FineListCreateView(generics.ListCreateAPIView):
 class FineDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Fine.objects.all()
     serializer_class = FineSerializer
+
+# List all members
+def member_list(request):
+    members = MemberProfile.objects.all()
+    return render(request, "member_list.html", {"members": members})
+
+
+# Member details
+def member_detail(request, pk):
+    member = get_object_or_404(MemberProfile, pk=pk)
+    return render(request, "member_detail.html", {"member": member})
+
+
+# List all clubs
+def club_list(request):
+    clubs = ReadingClub.objects.all()
+    return render(request, "club_list.html", {"clubs": clubs})
+
+
+# Club details
+def club_detail(request, pk):
+    club = get_object_or_404(ReadingClub, pk=pk)
+    return render(request, "club_detail.html", {"club": club})
+
+
+# List fines
+def fine_list(request):
+    fines = Fine.objects.all()
+    return render(request, "fine_list.html", {"fines": fines})
+
+
+# Fine details
+def fine_detail(request, pk):
+    fine = get_object_or_404(Fine, pk=pk)
+    return render(request, "fine_detail.html", {"fine": fine})
 
