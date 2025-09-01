9d314c27c5f60f4b4e1aaa86fb5cde7519020f68
473krija <krijashobi2003@gmail.com>
2025-09-01 17:50:13 +0530

    updated files

--- library_management/settings.py
+++ library_management/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'library_members',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     
 ]
 
@@ -52,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 # REST_FRAMEWORK = {
@@ -153,3 +156,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",  # Angular default dev server
+]
